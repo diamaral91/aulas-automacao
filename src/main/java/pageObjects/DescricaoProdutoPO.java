e33d06cee43dd24a5c05025f076eae115bd98522
Mírian Mailon Garcia <miriang@dbserver.com.br>
2020-07-16 20:54:01 -0300

    update

---
+++ src/main/java/pageObjects/DescricaoProdutoPO.java
@@ -0,0 +1,19 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class DescricaoProdutoPO {
+
+    private WebDriver driver;
+
+    public DescricaoProdutoPO(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void clicarNaVoltagem(){
+        driver.findElement(voltagem110).click();
+    }
+
+   private By voltagem110 = By.xpath("//span[@type='Voltagem' and text()='110 volts']");
+}
